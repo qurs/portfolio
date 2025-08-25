7fd2b56431bdf92829d3525864434c5a1aaed9ed
Ivan <vanya.schetinsky@yandex.ru>
2025-08-25 12:36:53 +0300

    Немного отредактировал описание "О себе"

--- src/app/AboutMeCard.tsx
+++ src/app/AboutMeCard.tsx
@@ -11,7 +11,7 @@ export default function AboutMeCard() {
 	return (
 		<div className='flex flex-col items-center justify-center p-4 bg-neutral-800 rounded-2xl border border-white/25'>
 			<h1 className={`text-sm xxs:text-lg xs:text-xl sm:text-xl md:text-2xl text-center mt-5 font-medium ${montserrat.className}`}>Иван Щетинский</h1>
-			<p className={`italic text-neutral-300 text-tiny tiny:text-xxs xxs:text-xs xs:text-sm sm:text-sm md:text-md text-center mt-5 font-normal ${montserrat.className}`}>Разработчик с 6-летним опытом самостоятельной работы над pet-проектами. Создавал игры, приложения и веб-сервисы с использованием Godot 4, Node.js, Next.js, PHP, TypeScript и Electron. Интересуюсь разработкой игр и современных веб-технологий, постоянно развиваю навыки и осваиваю новые инструменты</p>
+			<p className={`italic text-neutral-300 text-tiny tiny:text-xxs xxs:text-xs xs:text-sm sm:text-sm md:text-md text-center mt-5 font-normal ${montserrat.className}`}>Разработчик с 6-летним опытом самостоятельной работы над pet-проектами. Создавал приложения и веб-сервисы с использованием Node.js, Next.js, TypeScript. Практиковался в создании игр с использованием Godot 4, Love2D и Lua. Интересуюсь разработкой современных веб-технологий, постоянно развиваю навыки и осваиваю новые инструменты</p>
 			<br></br>
 			<div className='flex items-center justify-center gap-10'>
 				<Link href={'https://github.com/qurs'} target='_blank'>
